220f1a87b87ed58d913d2b007303902257d96652
zhengbrayden <zhengbrayden@gmail.com>
2024-12-24 07:24:06 -0500

    feat: Add lobby view and related URL pattern for lobby management

--- todos/urls.py
+++ todos/urls.py
@@ -3,12 +3,20 @@ from rest_framework.routers import DefaultRouter
 from rest_framework_simplejwt.views import TokenObtainPairView
 from django.urls import path
 from .views import CustomTokenObtainPairView
-from .views import TodoViewSet, RegisterView
-
-router = DefaultRouter()
-router.register(r'todos', TodoViewSet, basename='todo')
+from .views import TodoViewSet, RegisterView, LobbyView
 
 urlpatterns = [
     path('register/', RegisterView.as_view(), name='register'),
     path('login/', CustomTokenObtainPairView.as_view(), name='login'),
-] + router.urls
+    path('lobby/', LobbyView.as_view()), #for create, join, 
+
+# - `-call`: Call the current bet.
+# - `-create <LOBBY>`: Create a new lobby with a unique name.
+# - `-join <LOBBY>`: Join an existing lobby by name.
+# - `-fold`: Fold your current hand.
+# - `-info_lobby [LOBBY]`: Show all lobbies or players in a specific lobby.
+# - `-info`: Get a DM with your player info for the current channel.
+# - `-raise`: Raise the current bet.
+# - `-leave`: Leave the current lobby.
+# - `-start`: Start the game as the host.
+]
